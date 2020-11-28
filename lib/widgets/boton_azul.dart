a4bab2e9dba5152d25cb5d7dc84edc7ba051c253
Sebastian <joansebasfranco@hotmail.com>
2020-11-28 13:13:29 -0500

    Login y registro terminado

---
+++ lib/widgets/boton_azul.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class BotonAzul extends StatelessWidget {
+
+  final String texto;
+  final Function onPressed;
+
+  BotonAzul({
+    @required this.texto,
+    @required this.onPressed, 
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      elevation: 2,
+      highlightElevation: 5,
+      color: Colors.blue,
+      shape: StadiumBorder(),
+      onPressed: onPressed,
+      child: Container(
+        width: double.infinity,
+        height: 50,
+        child: Center(
+          child: Text(this.texto, style: TextStyle(color: Colors.white, fontSize: 16),),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/labels.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Labels extends StatelessWidget {
+
+  final String ruta;
+  final String titulo;
+  final String subTitulo;
+
+  const Labels({
+    Key key, 
+    @required this.ruta, 
+    @required this.titulo, 
+    @required this.subTitulo
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(this.titulo, style: TextStyle(color: Colors.black54, fontSize: 14.0, fontWeight: FontWeight.w300)),
+          SizedBox(height: 10.0,),
+          GestureDetector(
+            onTap: () => Navigator.pushReplacementNamed(context, this.ruta),
+            child: Text(this.subTitulo, style: TextStyle(color: Colors.blue[600], fontSize: 18.0, fontWeight: FontWeight.bold))
+          ),
+        ],
+      ),
+    );
+  }
+}
